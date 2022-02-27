6ba9f773c80eb7328009b180ff1fb8c20b9c2e9c
ahmed <904645@mof.gov.sa>
2022-02-27 22:35:25 +0200

    Add health checks

--- WebApp_MultipleEnvironments/Startup.cs
+++ WebApp_MultipleEnvironments/Startup.cs
@@ -23,6 +23,7 @@ namespace WebApp_MultipleEnvironments
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks();
             services.AddRazorPages();
             services.Configure<MyConfig>(Configuration.GetSection("MyConfig"));
             services.Configure<Test>(Configuration.GetSection("Test"));
@@ -48,6 +49,7 @@ namespace WebApp_MultipleEnvironments
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseHealthChecks("/health");
 
             app.UseAuthorization();
 
